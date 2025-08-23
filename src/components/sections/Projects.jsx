e8c313d583f62eb5c235bb9e681923093d719dc2
kamalkori28 <anshukori370@gmail.com>
2025-08-23 12:35:42 +0530

    Add new project title

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -124,6 +124,36 @@ management and responsive design for cross-device support</p>
               </div>
             </div>
 
+
+
+            {/* Project 5 */}
+            <div className="glass p-6 rounded-xl border border-white/10 hover:-translate-y-1 text-center glass hover:border-cyan-400 hover:shadow-[0_0_20px_rgba(34,211,238,0.6)] transition duration-300 ease-in-out">
+              <h3 className="text-xl font-bold mb-2 text-cyan-400">Jarvis</h3>
+              <p className="text-white mb-4">
+                Work in Progress......
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["Python"].map((tech) => (
+                  <span
+                    key={tech}
+                    className="bg-blue-500/10 text-cyan-400 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all"
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="https://github.com/kamalkori28/Jarvis"
+                  className="relative group text-white hover:text-cyan-400 transition-colors my-4"
+                  target="_blank"
+                >
+                  View Project →
+                  <span className="absolute left-0 -bottom-1 h-0.5 w-0 bg-cyan-400 transition-all duration-300 ease-in-out group-hover:w-full"></span>
+                </a>
+              </div>
+            </div>
+
             
           </div>
         </div>
