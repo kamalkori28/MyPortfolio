32ed0bf408e8752254b5cf9649f3822c51ebf70b
kamalkori28 <anshukori370@gmail.com>
2025-08-27 12:59:59 +0530

    add new project texttoimage-gen

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -99,37 +99,9 @@ management and responsive design for cross-device support</p>
 
 
 
-            {/* Project 4
-            <div className="glass p-6 rounded-xl border border-white/10 hover:-translate-y-1 text-center glass hover:border-cyan-400 hover:shadow-[0_0_20px_rgba(34,211,238,0.6)] transition duration-300 ease-in-out">
-              <h3 className="text-xl font-bold mb-2 text-cyan-400">E-Commerce Web App</h3>
-              <p className="text-white mb-4">
-                Responsive e-commerce frontend built using HTML, CSS, and JavaScript. Features include a modern UI, product listing, shopping cart functionality, and smooth user interactions.
-              </p>
-              <div className="flex flex-wrap gap-2 mb-4">
-                {["HTML", "CSS", "JS", "PHP"].map((tech, key) => (
-                  <span
-                    key={key}
-                    className="bg-blue-500/10 text-cyan-400 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all"
-                  >
-                    {tech}
-                  </span>
-                ))}
-              </div>
-              <div className="flex justify-between items-center">
-                <a
-                  href="https://github.com/kamalkori28/Ecommerce-Website"
-                  className="relative group text-white hover:text-cyan-400 transition-colors my-4"
-                  target="_blank"
-                >
-                  View Project →
-                  <span className="absolute left-0 -bottom-1 h-0.5 w-0 bg-cyan-400 transition-all duration-300 ease-in-out group-hover:w-full"></span>
-                </a>
-              </div>
-            </div>
-   */}
 
 
-            {/* Project 5 */}
+            {/* Project 4 */}
             <div className="glass p-6 rounded-xl border border-white/10 hover:-translate-y-1 text-center glass hover:border-cyan-400 hover:shadow-[0_0_20px_rgba(34,211,238,0.6)] transition duration-300 ease-in-out">
               <h3 className="text-xl font-bold mb-2 text-cyan-400">Jarvis</h3>
               <p className="text-white mb-4">
@@ -157,6 +129,36 @@ management and responsive design for cross-device support</p>
               </div>
             </div>
 
+
+
+            {/* Project 5 */}
+            <div className="glass p-6 rounded-xl border border-white/10 hover:-translate-y-1 text-center glass hover:border-cyan-400 hover:shadow-[0_0_20px_rgba(34,211,238,0.6)] transition duration-300 ease-in-out">
+              <h3 className="text-xl font-bold mb-2 text-cyan-400">Text To Image Generator </h3>
+              <p className="text-white mb-4">Work in Progress.....
+                {/* A web application that converts user-inputted text into images using AI models. Built with HTML, CSS, JavaScript, and PHP, it allows users to generate unique images from text prompts, download results, and explore creative possibilities. */}
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {[].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-cyan-400 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all"
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="https://github.com/kamalkori28/TextToImage-gen"
+                  className="relative group text-white hover:text-cyan-400 transition-colors my-4"
+                  target="_blank"
+                >
+                  View Project →
+                  <span className="absolute left-0 -bottom-1 h-0.5 w-0 bg-cyan-400 transition-all duration-300 ease-in-out group-hover:w-full"></span>
+                </a>
+              </div>
+            </div>
+  
             
           </div>
         </div>
