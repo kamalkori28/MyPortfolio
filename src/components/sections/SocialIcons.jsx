382cbf2c4e7b3478a15c7f5c380cee3c031a0ab4
kamalkori28 <anshukori370@gmail.com>
2025-09-11 19:03:11 +0530

    ads

--- src/components/sections/SocialIcons.jsx
+++ src/components/sections/SocialIcons.jsx
@@ -5,8 +5,7 @@ import { FaInstagram } from 'react-icons/fa';
 
 export const SocialIcons = () => {
   return (
-    <div className="flex justify-center gap-10 p-6">
-      {/* GitHub */}
+    <div className="flex flex-wrap justify-center gap-17 sm:gap-10 md:gap-12 p-4 sm:p-6">
       <a
         href="https://github.com/kamalkori28"
         target="_blank"
@@ -16,17 +15,15 @@ export const SocialIcons = () => {
         <FaGithub size={24} color="#181717" />
       </a>
 
-      {/* X / Twitter */}
       <a
         href="https://www.instagram.com/_anshu__kori"
         target="_blank"
         rel="noopener noreferrer"
-        className="p-3 rounded-xl bg-white border border-gray-300 hover:scale-110 hover:shadow-md transition-transform duration-300 ease-in-out "
+        className="p-3 rounded-xl bg-white border border-gray-300 hover:scale-110 hover:shadow-md transition-transform duration-300 ease-in-out"
       >
         <FaInstagram size={24} color="#E4405F" />
       </a>
 
-      {/* LinkedIn */}
       <a
         href="https://www.linkedin.com/in/kamalkori28"
         target="_blank"
